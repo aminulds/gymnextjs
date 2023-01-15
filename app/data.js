0a3da54fc21520e1478983de563d69055bb37ed8
aminulds <aminulds61@gmail.com>
2023-01-15 17:47:16 +0600

    home page data file created

---
+++ app/data.js
@@ -0,0 +1,96 @@
+import { MdWaterDrop, MdFitnessCenter } from 'react-icons/md';
+import img1 from '../public/images/latestUpload/1.png';
+import img2 from '../public/images/latestUpload/2.png';
+import img3 from '../public/images/latestUpload/3.png';
+
+export const uploadData = [
+    {
+        type: 'latestUpload',
+        items: [
+            {
+                categoryIcon: <MdWaterDrop />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img1,
+            },
+        ],
+    },
+
+    {
+        type: 'event',
+        items: [
+            {
+                categoryIcon: <MdWaterDrop />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img2,
+            },
+            {
+                categoryIcon: <MdFitnessCenter />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img3,
+            },
+        ],
+    },
+
+    {
+        type: 'video',
+        items: [
+            {
+                categoryIcon: <MdWaterDrop />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img2,
+            },
+            {
+                categoryIcon: <MdFitnessCenter />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img3,
+            },
+            {
+                categoryIcon: <MdWaterDrop />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img2,
+            },
+            {
+                categoryIcon: <MdFitnessCenter />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img3,
+            },
+        ],
+    },
+
+    {
+        type: 'discussions',
+        items: [
+            {
+                categoryIcon: <MdWaterDrop />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img2,
+            },
+            {
+                categoryIcon: <MdFitnessCenter />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img3,
+            },
+            {
+                categoryIcon: <MdWaterDrop />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img2,
+            },
+            {
+                categoryIcon: <MdFitnessCenter />,
+                title: 'Title here',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                image: img3,
+            },
+        ],
+    },
+];
--- app/page.js
+++ app/page.js
@@ -1,8 +1,68 @@
 'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { uploadData } from './data';
+
 const Home = () => {
+    const latestUpload = uploadData.filter((latesItems) => {
+        return latesItems.type === 'latestUpload';
+    });
+
+    const eventData = uploadData.filter((eventItem) => {
+        return eventItem.type === 'event';
+    });
+
     return (
         <div className="col-12 col-md-9 col-lg-10">
-            <h1>Home</h1>
+            <div className="row">
+                <div className="col-lg-6">
+                    <div className="sectionTitle d-flex align-items-center justify-content-between">
+                        <h4>Latest Uploads</h4>
+                    </div>
+
+                    {latestUpload.map((item) => (
+                        <>
+                            {item.items.map((item) => (
+                                <div className="card p-3 p-lg-4">
+                                    <div className="d-flex align-items-center justify-content-between">
+                                        <h6 className="card-title">{item.title}</h6>
+                                        <span className="bg-primary p-2 text-white d-flex align-items-center justify-content-center rounded-circle">{item.categoryIcon}</span>
+                                    </div>
+                                    <p>{item.description}</p>
+                                    <Image className="img-fluid" src={item.image} />
+                                </div>
+                            ))}
+                        </>
+                    ))}
+                </div>
+
+                <div className="col-lg-6">
+                    <div className="sectionTitle d-flex align-items-center justify-content-between">
+                        <h4>Upcoming Events</h4>
+                        <Link href="#">All Events</Link>
+                    </div>
+
+                    <div className="row">
+                        {eventData.map((item) => (
+                            <>
+                                {item.items.map((item) => (
+                                    <div className="col">
+                                        <div className="card p-3 p-lg-4">
+                                            <Image className="img-fluid" src={item.image} />
+                                            <div className="d-flex align-items-center justify-content-between">
+                                                <h6 className="card-title">{item.title}</h6>
+                                                <span>{item.categoryIcon}</span>
+                                            </div>
+                                            <p>{item.description}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
