2ce4e39db9533fc8c5486f597a744b5c2189ffbe
aminulds <aminulds61@gmail.com>
2023-01-15 13:53:16 +0600

    auth page updated

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -7,42 +7,40 @@ import Button from '../../../components/Input/Button';
 
 const SignIn = () => {
     return (
-        <div className="auth py-4 py-md-0">
-            <div className="container vh-100">
-                <div className="row vh-100 justify-content-center">
-                    <div className="col-12 col-md-9 d-flex align-items-center">
-                        <div className="row g-4 g-md-5 row-cols-1 row-cols-md-2 flex-md-row-reverse">
-                            <div className="col d-flex align-items-center">
-                                <div className="authBannerWrap">
-                                    <Image className="banner img-fluid" src={imgAuthBanner} alt="Auth Banner Image" />
-                                </div>
+        <div className="container vh-100">
+            <div className="row vh-100 justify-content-center">
+                <div className="col-12 col-md-9 d-flex align-items-center">
+                    <div className="row g-4 g-md-5 row-cols-1 row-cols-md-2 flex-md-row-reverse">
+                        <div className="col d-flex align-items-center">
+                            <div className="authBannerWrap">
+                                <Image className="banner img-fluid" src={imgAuthBanner} alt="Auth Banner Image" />
                             </div>
+                        </div>
 
-                            <div className="col d-flex align-items-center">
-                                <div className="authFormWrap">
-                                    <Image className="logo img-fluid mb-4 mb-md-5" src={imgLogo} />
-                                    <h5 className="logInTitle text-uppercase mb-4">Login to your account</h5>
+                        <div className="col d-flex align-items-center">
+                            <div className="authFormWrap">
+                                <Image className="logo img-fluid mb-4 mb-md-5" src={imgLogo} />
+                                <h5 className="logInTitle text-uppercase mb-4">Login to your account</h5>
 
-                                    <form className="mb-3">
-                                        <Input type="email" name="email" id="email" placeholder="Username / Email" />
-                                        <Input type="password" name="password" id="password" placeholder="Password" />
-                                        <Button type="button" text="Login to Your Account" id="logIn" />
-                                    </form>
-                                    <Link className="nav-link mb-2" href="/auth/signup">
-                                        or Sign up today !
-                                    </Link>
-                                </div>
+                                <form className="mb-3">
+                                    <Input type="email" name="email" id="email" placeholder="Username / Email" />
+                                    <Input type="password" name="password" id="password" placeholder="Password" />
+                                    <Button type="button" text="Login to Your Account" id="logIn" />
+                                </form>
+                                <Link className="nav-link mb-2" href="/auth/signup">
+                                    or Sign up today !
+                                </Link>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    {/* Auth Footer */}
-                    <div className="authFooter d-flex align-items-center justify-content-between">
-                        <Link href="#" className="nav-link">
-                            Privacy Policy
-                        </Link>
-                        <span className="copyright">Copyright © {new Date().getFullYear()}</span>
-                    </div>
+                {/* Auth Footer */}
+                <div className="authFooter d-flex align-items-center justify-content-between">
+                    <Link href="#" className="nav-link">
+                        Privacy Policy
+                    </Link>
+                    <span className="copyright">Copyright © {new Date().getFullYear()}</span>
                 </div>
             </div>
         </div>
